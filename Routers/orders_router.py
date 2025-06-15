bff8bca5eefad97116fc3719a50267ff26c21c37
udivak <udivak9@gmail.com>
2025-06-15 11:07:29 +0300

    implemented raw sql query handling

--- Routers/orders_router.py
+++ Routers/orders_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from models import OrderRequest, OrderItem
+from models import OrderRequest, OrderItem, OrderIDs
 import data_access
 
 router = APIRouter()
@@ -17,6 +17,33 @@ async def create_order(order_request: OrderRequest):
     new_order_id = data_access.insert_order_items(order_request)
     upsell_items = data_access.find_upsells(order_request, new_order_id)
     return { "message": "Order created successfully", "upsell_items": upsell_items }
+
+
+@router.get("/get_order_items_info/{order_id}")
+async def get_order_items_info(order_id: int):
+    order_items = data_access.get_order_items_info(order_id)
+    if len(order_items) == 0:
+        raise HTTPException(status_code=404, detail={ "error": "An error has occurred" })
+    return order_items
+
+
+@router.post("/get_multiple_order_items_info")
+async def get_multiple_order_items_info(order_ids_data: OrderIDs):
+    all_items = []
+    for order_id in order_ids_data.order_ids:
+        order_items = data_access.get_order_items_info(order_id)
+        if order_items:
+            all_items.append({
+                "order_id": order_id,
+                "items": order_items
+            })
+        else:
+            all_items.append({
+                "order_id": order_id,
+                "items": [],
+                "error": "No items found or invalid order ID"
+            })
+    return all_items
 
 
 # upsell test endpoint
